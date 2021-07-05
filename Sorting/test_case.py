0f31c215cc91e9b9fafa241fa72429c0ef13e301
Julia Ustits <july-smille@mail.ru>
2021-07-05 10:07:49 +0300

    add Counting sort

--- Sorting/test_case.py
+++ Sorting/test_case.py
@@ -11,6 +11,7 @@ test_case_sort = [
     [float('inf')],
     [float('-inf')],
     [sys.maxsize],
+    [sys.maxsize - 10, sys.maxsize, sys.maxsize - 2],
     [True, False, True],
     [None],
     [True, 10],
@@ -18,6 +19,30 @@ test_case_sort = [
 ]
 
 test_case_value = [
+    [1, 'a', None, True],
+    ['asd', True]
+]
+
+test_case_sort_int = [
+    [],
+    [-1, -1, -1, -1],
+    [0],
+    [3, 2, 1],
+    [-1, -2, -3, -4, -5],
+    [1, 2, 3, 4, 5],
+    [True, False, True],
+    [sys.maxsize],
+    [True, 10]
+]
+
+test_case_value_int = [
+    [1.1, 1],
+    ['test', 'c'],
+    ['t', 'a', 'v', 'r', 'z'],
+    [1.1, 1],
+    [float('inf')],
+    [float('-inf')],
+    [None],
     [1, 'a', None, True],
     ['asd', True]
 ]
